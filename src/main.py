70b1efb2bdf672ea3ff9ec836e8b103471d3ef95
Darío Chinea Delgado <dariocd0808@gmail.com>
2022-11-09 12:48:37 +0000

    Feature: Se añadió nueva funcionlidad
    Se añadió el sleep para que se muestre el mensaje de que murio el hijo, a los 5 segundos

--- src/main.py
+++ src/main.py
@@ -2,15 +2,15 @@ import os
 from time import sleep
 
 def padre():
-    num = int(input("Cuantos hijos quieres tener UwU: "))
+    num = int(input("¿Cuántos procesos hijos quieres crear?: "))
     for i in range(num):
         newPid = os.fork()
         if newPid == 0:
             hijo()
 
-
 def hijo():
     print("Soy el porceso HIJO con PID :  %d" % os.getpid())
+    sleep(5)
     print("Muerto porceso hijo con PID : %d" %os.getpid())
     os._exit(0)
 
